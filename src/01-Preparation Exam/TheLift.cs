0295658eb20149f7573aeda761398b60e7539fcb
ivannakov05 <ivannakov11@gmail.com>
2025-06-20 22:39:22 +0300

    feat(01-Preparation exam): add The Lift exercise solution

---
+++ src/01-Preparation Exam/TheLift.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftuniCSharpMidExamPreparation
+{
+    class TheLift
+    {
+        static void Main()
+        {
+            int peopleWaitingForLift = int.Parse(Console.ReadLine());
+            int[] peopleInEveryWagon = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            bool isThereEmptySeat = false;
+            for(int i = 0; i < peopleInEveryWagon.Length; i++)
+            {
+                while (peopleInEveryWagon[i] < 4 && peopleWaitingForLift > 0)
+                {
+                    peopleInEveryWagon[i]++;
+                    peopleWaitingForLift--;
+                }
+            }
+            for(int i = 0; i<peopleInEveryWagon.Length;i++)
+            {
+                if (peopleInEveryWagon[i]!=4)
+                {
+                    isThereEmptySeat=true;
+                    break;
+                }
+            }
+            if(peopleWaitingForLift == 0 && isThereEmptySeat)
+            {
+                Console.WriteLine("The lift has empty spots!");
+            }
+            else if(peopleWaitingForLift > 0 &&  !isThereEmptySeat)
+            {
+                Console.WriteLine($"There isn't enough space! {peopleWaitingForLift} people in a queue!");
+            }
+            Console.WriteLine(string.Join(" ",peopleInEveryWagon));
+        }
+    }
+}
