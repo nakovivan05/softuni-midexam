d094d14f4586f9d6993091404a7bde48c3c2bdc5
ivannakov05 <ivannakov11@gmail.com>
2025-06-21 17:39:46 +0300

    feat(02-Preparation Exam): add Shooting For the Win exercise solution

---
+++ src/02-Preparation Exam/ShootingForTheWin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftUniCSharpMidExamPreparation
+{
+    class ShootingForTheWin
+    {
+        static void Main()
+        {
+            int[] targets = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int countOfShotTargets = 0;
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                int index = int.Parse(input);
+                if (index >= 0 && index < targets.Length)
+                {
+                    if (targets[index] != -1)
+                    {
+                        int shotValue = targets[index];
+                        targets[index] = -1;
+                        countOfShotTargets++;
+                        for (int i = 0; i < targets.Length; i++)
+                        {
+                            if (targets[i] != -1)
+                            {
+                                if (targets[i] > shotValue)
+                                {
+                                    targets[i] -= shotValue;
+                                }
+                                else
+                                {
+                                    targets[i] += shotValue;
+                                }
+                            }
+
+
+                        }
+                        
+                    }
+                }
+            }
+            Console.Write($"Shot targets: {countOfShotTargets} -> ");
+            Console.WriteLine(string.Join(" ", targets));
+        }
+    }
+}
