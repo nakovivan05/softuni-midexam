a85afbb95e3192556c4209d16fe7be600bd8e67f
ivannakov05 <ivannakov11@gmail.com>
2025-06-22 22:33:25 +0300

    feat(03-Regular Mid Exam): add Cooking Masterclass exercise solution

---
+++ src/Regular Mid Exam - 22.06.2025/CookingMasterclass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace SoftUniCSharpRegularMidExam
+{
+    class Program
+    {
+        static void Main()
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int countOfStudents = int.Parse(Console.ReadLine());
+            double priceForPackageOfFlour = double.Parse(Console.ReadLine());
+            double priceForSingleEgg = double.Parse(Console.ReadLine());
+            double priceForSingleApron = double.Parse(Console.ReadLine());
+            double finalPriceForFlourPackages = (countOfStudents - countOfStudents / 5) * priceForPackageOfFlour;
+            double finalPriceForAprons = Math.Ceiling(countOfStudents * 1.2) * priceForSingleApron;
+            double finalPriceForEggs = countOfStudents * 10 * priceForSingleEgg;
+            double totalPrice = finalPriceForAprons + finalPriceForEggs + finalPriceForFlourPackages;
+            if (totalPrice <= budget)
+            {
+                Console.WriteLine($"Items purchased for {totalPrice:F2}$.");
+            }
+            else
+            {
+                Console.WriteLine($"{(totalPrice - budget):F2}$ more needed.");
+            }
+        }
+    }
+}
